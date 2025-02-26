10f9ba265c6a8cddfe5362dbf66f77933edb2aeb
phuoc <connguaho@gmail.com>
2025-02-26 16:56:36 +0700

    fix: bug ci cd

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Card () {
+    return (
+        <>
+        <div className="text-black">
+        day la card
+        </div>
+        </>
+    );
+}
